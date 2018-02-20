e571b6d4be825840314b29ddfa529d5e084945e9
Tiratdunpont <ierairecio@gmail.com>
2018-02-20 12:48:18 +0100

    Test2

--- hola2/src/hola2/hola2.java
+++ hola2/src/hola2/hola2.java
@@ -2,7 +2,39 @@ package hola2;
 
 public class hola2 {
 
-	
+	public static void main (String [] args) throws ClassNotFoundException {
+        FileOutputStream grafs = null;
+        ObjectOutputStream sortida = null;
+        FileInputStream frafs = null;
+        ObjectInputStream entrada = null;
+        Alumne a;
+        Data f;
+        try {
+            grafs = new FileOutputStream ("data/alumnos.dat");
+            sortida = new ObjectOutputStream (grafs);
+            f = new Data (5,9,2011);
+            a = new Alumne ("12345678A", "Lucas González", 20, f);
+            sortida.writeObject (a);
+            f = new Data (7,9,2011);
+            a = new Alumne ("98765432B", "Anacleto Jiménez", 19, f);
+            sortida.writeObject (a);
+            f = new Data (8,9,2011);
+            a = new Alumne ("78234212Z", "Maria Zapata", 21, f);
+            sortida.writeObject (a);
+           
+        } catch (FileNotFoundException e) {
+            System.out.println (e.getMessage ());
+        } catch (IOException e) {
+            System.out.println (e.getMessage ());
+        } finally {
+            try {
+                if (grafs != null) grafs.close ();
+                if (sortida != null) sortida.close ();
+            } catch (IOException e) {
+                System.out.println (e.getMessage ());
+            }
+        }
+    }
 	
 	
 	
